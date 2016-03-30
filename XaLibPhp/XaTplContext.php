6cfc31d597e1ad4d85332e006334c65738a845c0
Alessandro Mariotti <alex@xallegro.com>
2016-03-30 19:26:32 +0100

    bug fixes

--- XaLibPhp/XaTplContext.php
+++ XaLibPhp/XaTplContext.php
@@ -14,7 +14,7 @@ class XaTplList extends XaTpl{
 
     }
 
-    protected function GetDropDown(array &$WsData,XaLibHttp &$HTTP,$RowId ):string {
+    protected function Context(array &$WsData,XaLibHttp &$HTTP):string {
 
         $JsCallOptions='{ResponseType:&quot;Html&quot;,TargetId:&quot;detail&quot;,JsEval:&quot;yes&quot;}';
 
@@ -26,64 +26,15 @@ class XaTplList extends XaTpl{
 
         return $Content;
     }
-
-    public function List(array $Conf,XaLibHttp &$HTTP,array &$WsData,$TplParams=""):string {
-
-        $actions="no";
-        $actions=$this->GetTplParam($HTTP,$TplParams,"actions");
-        
-        $Title="Titolo Lista";
-        $Title=$this->GetTplParam($HTTP,$TplParams,"title");
-        
-
-        $Content='<table class="list">';
-      
-
-        $Content.='<tr class="title"><th colspan="100%"><span>'.$Title.'</span><ul class="RightToolbar"><li class="FunctionIcon Refresh"></li><li class="FunctionIcon Expand"></li></ul></th></tr>';
-
-        $ItemsNumber=count($WsData['list']['item']);
-
-      if ($ItemsNumber>0) {
-
-        /*ADDING TITLES*/
-        $Content.='<tr class="header">';
-       
-        foreach($WsData['list']['item'][0] as $key => $value) {
-            $Content.='<th>'.$key.'</th>';
-        } 
-        
-        if ($actions=="yes"){
-            $Content.='<th>Actions</th>';
-        }
-
-        $Content.='</tr>';
-
-        for ($i=0;$i<$ItemsNumber;$i++) {
-            
-            $Content.='<tr class="row">';
-         
-            foreach($WsData['list']['item'][$i] as $value) { 
-
-                $Content.='<td>'.$value.'</td>'; 
-            }
-          
-            //ADDING ACTIONS
-            if ($actions=="yes") {
-                
-                $Content.='<td>';
-                $RowId=$WsData['list']['item'][$i]['id'];
-                $Content.=$this->GetDropDown($WsData,$HTTP,$RowId);
-                $Content.='</td>';
-            }
-        }
-
-    } else {
-        $Content.='<tr><td>No data</td></tr>';
-    }
-
-        $Content.="</table>";
-        return $Content;
-    }
+    
+    
+    /*
+    $actions="no";
+    $actions=$this->GetTplParam($HTTP,$TplParams,"actions");
+    
+    $Title="Titolo Lista";
+    $Title=$this->GetTplParam($HTTP,$TplParams,"title");
+    */
 }
 
 ?>
